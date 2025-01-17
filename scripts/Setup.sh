257a5ca6471bc5b82429810ec4353c3cc59afa69
Moonm4nn <mca@njit.edu>
2025-01-17 11:58:09 +0000

    Fixing linux build script

--- scripts/Setup.sh
+++ scripts/Setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ..
-Walnut/vendor/bin/premake/Linux/premake5.lua --cc=clang --file=Build-Server.lua gmake2
+Walnut/vendor/bin/premake/Linux/premake5 --cc=clang --file=Build-Server.lua gmake2
 popd
